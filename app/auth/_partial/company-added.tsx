57cdb6631e691ae017a9b6157d1d5c06f0b3c9bb
gabrielolivra <gabernardinooliveira@gmail.com>
2025-04-26 08:07:16 -0300

    adjust table

--- app/auth/_partial/company-added.tsx
+++ app/auth/_partial/company-added.tsx
@@ -10,31 +10,31 @@ export default function CompanyAdded() {
   return (
     <div>
       <Button tipo="success" className="w-[300px] ml-auto py-2 mb-4">Adicionar Empresa</Button>
-      <div className="overflow-x-auto overflow-y-auto p-4 w-[95vw]  border border-gray-300 rounded-lg shadow-md">
-        <table className="min-w-full border-collapse border border-gray-200">
+      <div className="overflow-x-auto overflow-y-auto p-4 w-[95vw]  rounded-lg shadow-md">
+        <table className="min-w-full ">
           <thead>
-            <tr className="bg-gray-100">
-              <th className="border border-gray-300 px-4 py-2 text-left text-sm font-medium text-gray-700">
+            <tr className="">
+              <th className="border-b font-bold border-b-gray-300 px-4 py-2 text-left text-sm text-gray-700">
                 Empresa
               </th>
-              <th className="border border-gray-300 px-4 py-2 text-left text-sm font-medium text-gray-700">
+              <th className="border-b border-b-gray-300 px-4 py-2 text-left text-sm font-bold text-gray-700">
                 Qt de Locais
               </th>
-              <th className="border border-gray-300 px-4 py-2 text-left text-sm font-medium text-gray-700">
+              <th className="border-b border-b-gray-300 px-4 py-2 text-left text-sm font-bold text-gray-700">
                 Ações
               </th>
             </tr>
           </thead>
           <tbody>
             {companies.map((company, index) => (
-              <tr key={index} className="">
-                <td className="border border-gray-300 px-4 py-2 text-sm text-gray-700">
+              <tr key={index} className="border-b border-b-gray-300">
+                <td className=" px-4 py-2 text-sm text-gray-700">
                   {company.name}
                 </td>
-                <td className="border border-gray-300 px-4 py-2 text-sm text-gray-700">
+                <td className=" px-4 py-2 text-sm text-gray-700">
                   {company.website}
                 </td>
-                <td className="border border-gray-300 px-4 py-2 text-sm text-gray-700">
+                <td className=" px-4 py-2 text-sm text-gray-700">
                   <div>botoes</div>
                 </td>
               </tr>
