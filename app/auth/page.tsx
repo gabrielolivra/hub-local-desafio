e64977ffbf11dc9711daa6b5dc0a3afe2b641bf8
gabrielolivra <gabernardinooliveira@gmail.com>
2025-04-29 21:35:37 -0300

    adjust modal

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,24 +1,14 @@
 'use client'
-import { useEffect, useState } from "react";
 import MyCompanys from "./_partial/my-companys";
-import { ICompany } from "../lib/contracts/companies/companies.contract";
 
 export default function Page() {
-  const [companies, setCompanies] = useState<ICompany[]>([]);
-
-  const handleCompaniesUpdate = (updatedCompanies: ICompany[]) => {
-    setCompanies(updatedCompanies);
-  };
-
-  useEffect(() => {
-  }, [companies])
 
   return (
     <main
       style={{ height: 'calc(100vh - 64px)' }}
       className="flex flex-col items-center justify-center h-screen bg-gray-200"
     >
-      <MyCompanys onCompaniesUpdate={handleCompaniesUpdate} />
+      <MyCompanys />
     </main>
   );
 }
